3823c48f4601ef544f83a6cc30162d47c46a8839
Holger Lech (uid02589) <holger.lech@continental-corporation.com>
2021-10-22 14:13:10 +0200

    uid02589 : Initialising Fucntion rand(), display 10 PWs

--- C_Programming/10_Hlech65730/Random_PW_Gen/main.c
+++ C_Programming/10_Hlech65730/Random_PW_Gen/main.c
@@ -1,13 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <windows.h>
 
 int main()
-{int c,n;
+{int c,n,m;
+ srand( (unsigned) time(NULL) ) ;
 	printf("random pw\n");
-	for(c=1;c<=10;c++)
-	{
-	n = rand()%100+33;
-	printf("%c\n",n);
+	for (m=1;m<=10;m++)
+	{ printf(" \n");
+        for(c=1;c<=10;c++)
+            {
+                n = rand()%100+33;
+                printf("%c",n);
+            }
+	  printf(" \n");
 	}
 	return 0;
 }
